62ef1700086ce3b3bac32da4c9c7e38c2f270ba9
Danh Nguyen <danh.nguyen@onsolve.com>
2019-12-26 11:58:45 +0700

    fix some syntax

--- webpack.config.js
+++ webpack.config.js
@@ -72,8 +72,6 @@ module.exports = {
       Promise: 'es6-promise-promise',
     }),
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: devMode ? '[name].css' : '[name].[hash].css',
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
     }),
